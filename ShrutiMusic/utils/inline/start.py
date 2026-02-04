aa3817048cc9f2b2b8ff96ae20ea4e2ea880441d
Heyynitin <heyyyynitin@gmail.com>
2026-02-04 07:50:28 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -84,11 +84,6 @@ def owner_panel(_):
     buttons = [
         [
             InlineKeyboardButton(text=_["S_H_1"], url=config.INSTAGRAM),
-            InlineKeyboardButton(text=_["S_H_2"], url=config.YOUTUBE),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_H_3"], url=config.GITHUB),
-            InlineKeyboardButton(text=_["S_H_4"], url=config.DONATE),
         ],
         [
             InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
