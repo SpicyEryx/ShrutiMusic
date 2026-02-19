f710f1a859a42689245b22a81f1079b6af0554c7
ᯓ‌𓆰𝅃 𝚬 ᰻⃪᱂ ꪗ ꪎ 𝁘 🇷🇺 𓆪ꪾ⇢ <heyyyynitin@gmail.com>
2026-02-19 07:18:32 +0530

    Update welcome.py

--- ShrutiMusic/plugins/tools/welcome.py
+++ ShrutiMusic/plugins/tools/welcome.py
@@ -122,7 +122,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             caption=f"""<b>❅──✦ Ꮗ 𝒆 𝒍 𝑪 𝒐 𝑴 𝒆 ✦──❅</b>
 
 ◐◑◐◑◐◑◐◑◐◑◐◑◐◑◐◐◑\n<blockquote>➻ <b>Gʀᴏᴜᴘ»</b> {member.chat.title}</blockquote>\n❅─────✧❅✦❅✧─────❅
-<blockquote><a href=\"https://t.me/ERYX_MUSIC_BOT?start=start\">💗 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🪄ㅤ</a></blockquote>\n<blockquote>➻ <b>Nᴀᴍᴇ»</n> {user.mention}!
+<a href=\"https://t.me/ERYX_MUSIC_BOT?start=start\">💗 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🪄</a>\n<blockquote>➻ <b>Nᴀᴍᴇ»</n> {user.mention}!
 ➻ <b>Yᴏᴜʀ ɪᴅ»</b> <code>{user.id}</code>
 ➻ <b>Usᴇʀɴᴀᴍᴇ»</b> @{user.username if user.username else "ɴᴏᴛ sᴇᴛ"}</blockquote>\n❅─────✧❅✦❅✧─────❅\n<blockquote><b>❖  𝐌ᴧᴅє  𝐁ɣ ➛  <a href=\"https://t.me/HEART_STR1NGS/114\">𓍼 ໋໋໋໋݊݊݊݊ 𝑬𝒓𝒚𝒙 𓆪ꪾ𓂃ᤢᤢㅤ</a></b></blockquote>
 <b>❅─────✧❅✦❅✧─────❅\n<blockquote>➻ Tʜᴀɴᴋs Fᴏʀ Jᴏɪɴɪɴɢ Us 🤍🌿</blockquote>\n◐◑◐◑◐◑◐◑◐◑◐◑◐◑◐◐◑</b>""",
